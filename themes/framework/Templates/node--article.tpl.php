2598d7d7938635e300a48f6d8a081399a55628d5
all4senses <all4senses@gmail.com>
2012-08-29 07:17:08 +0300

    Mws

--- themes/framework/Templates/node--article.tpl.php
+++ themes/framework/Templates/node--article.tpl.php
@@ -1,3 +1,10 @@
+
+<?php if ($page): ?>
+  <iframe width="560" height="315" src="http://mws.all4senses.com/mws/catalog" frameborder="0"></iframe>
+<?php endif; ?>
+
+
+
 <?php if (!$page): ?>
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 <?php endif; ?>
