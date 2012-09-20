f92649cd47a1074e2514a860b10be0c829be8286
all4senses <all4senses@gmail.com>
2012-09-20 19:03:03 +0300

    Park modules and theme

--- themes/park/Templates/node--album.tpl.php
+++ themes/park/Templates/node--album.tpl.php
@@ -1,13 +1,7 @@
 <?php 
 
-//dpm($node); dpm($content);
-
-//hide($content['body']);
-hide($content['field_body_to_gallery']);
 hide($content['comments']);
 hide($content['links']);
-
-print render($title_prefix); 
 
 if (!$page) {
   echo '<h2 class="title"' , $title_attributes , '><a href="' , $node_url, '" title="' , $title , '">' , $title , '</a></h2>';
@@ -15,7 +9,6 @@ if (!$page) {
 else {
   echo '<h2 class="title"' , $title_attributes , '>' , $title , '</h2>';
 }
-print render($title_suffix); 
 
 echo '<div class="meta">';
     if ($display_submitted) {
@@ -39,18 +32,6 @@ echo '<div class="content"' , $content_attributes. '>';
         echo '<div class="mode"><a href="' , $node_url, '?t=full" title="' , t('View in full') , '">View in full</a></div>';
         hide($content['body']);
         echo $node->field_body_to_gallery['und'][0]['value'];
-
-//        drupal_add_js('sites/all/libraries/jquery.plugins/Ad-gallery/jquery.ad-gallery.js');
-//        drupal_add_css('sites/all/libraries/jquery.plugins/Ad-gallery/css/jquery.ad-gallery.css', array('preprocess' => FALSE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
-//        
-//        //1, 3, 4, 
-//        drupal_add_js('sites/all/libraries/jquery.plugins/Colorbox/jquery.colorbox.js');
-//        drupal_add_css('sites/all/libraries/jquery.plugins/Colorbox/css4/colorbox.css', array('preprocess' => FALSE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
-//        
-//        $path_to_module = drupal_get_path('module', 'park_image');
-//        drupal_add_js($path_to_module . '/js/park_add_Colorbox_ToAlbums.js');
-//        drupal_add_js($path_to_module . '/js/park_add_Gallery-ad_ToAlbums.js');
-//        drupal_add_css($path_to_module . '/css/park_add_Gallery-ad_ToAlbums.css', array('preprocess' => FALSE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
   
         drupal_add_js(array('park_image' => array('group_' . $node->nid => 1)), 'setting');
     }
--- themes/park/Templates/node--master_class.tpl.php
+++ themes/park/Templates/node--master_class.tpl.php
@@ -1,21 +1,13 @@
 <?php 
 
-//if($view_mode == 'teaser') echo 'Teaser<br>';
-  
-//dpm($node);
-//dpm($content);
-
-//hide($content['body']);
-hide($content['field_body_to_gallery']);
 hide($content['comments']);
 hide($content['links']);
 
-print render($title_prefix); 
+
 if (!$page)
     echo '<h2 class="title"' , $title_attributes , '><a href="' , $node_url, '" title="' , $title , '">' , $title , '</a></h2>';
 else
     echo '<h2 class="title"' , $title_attributes , '>' , $title , '</h2>';
-print render($title_suffix); 
 
 echo '<div class="meta">';
     if ($display_submitted)
@@ -39,38 +31,7 @@ echo '<div class="content"' , $content_attributes. '>';
         echo '<div class="mode"><a href="' , $node_url, '?t=full" title="' , t('View in full') , '">View in full</a></div>';
         hide($content['body']);
         echo $node->field_body_to_gallery['und'][0]['value'];
-        
-        //drupal_add_css("sites/all/modules/_Park/park_image/css/ad/jquery.ad-gallery.css", array('preprocess' => FALSE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
-        //drupal_add_css("sites/all/modules/_Park/park_image/css/ad/jquery.ad-gallery_custom.css", array('preprocess' => FALSE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
-        drupal_add_css("sites/all/libraries/jquery.plugins/Ad-gallery/css/jquery.ad-gallery.css", array('preprocess' => FALSE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
-        drupal_add_css("sites/all/libraries/jquery.plugins/Ad-gallery/css/jquery.ad-gallery_custom.css", array('preprocess' => FALSE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
-
-        //drupal_add_js('sites/all/modules/_Park/park_image/js/ad/a4s_image_gallery_ad.js');
-        //drupal_add_js('sites/all/modules/_Park/park_image/js/ad/jquery.ad-gallery.js');
-        drupal_add_js('sites/all/libraries/jquery.plugins/Ad-gallery/a4s_image_gallery_ad.js');
-        drupal_add_js('sites/all/libraries/jquery.plugins/Ad-gallery/jquery.ad-gallery.js');
-        
-        
-        //1, 3, 4, 
-        drupal_add_css("sites/all/libraries/jquery.plugins/Colorbox/css4/colorbox.css", array('preprocess' => FALSE)); // array('group' => CSS_THEME, 'preprocess' => FALSE)
-        drupal_add_js('sites/all/libraries/jquery.plugins/Colorbox/jquery.colorbox.js');
-        
-        drupal_add_js('
-            
-                    jQuery(document).ready(function(){
-                            //Examples of how to assign the ColorBox event to elements
-                            jQuery(".group_' . $node->nid .'").colorbox({transition:"fade", slideshow:true, slideshowAuto: false, rel:"group_' . $node->nid . '", width:"95%", height:"95%"});
-
-                            //$(".group2").colorbox({rel:"group2", transition:"fade"});
-                            //$(".group3").colorbox({rel:"group3", transition:"none", width:"75%", height:"75%"});
-                            //$(".group4").colorbox({rel:"group4", slideshow:true});
-                    });
-            
-
-
-                ', 'inline');
-        
-        
+        drupal_add_js(array('park_image' => array('group_' . $node->nid => 1)), 'setting');
     }
 
     // in teaser somehow direct render($content) doesn't show body
