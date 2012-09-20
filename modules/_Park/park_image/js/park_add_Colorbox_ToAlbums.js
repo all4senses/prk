098519dda25e6f3fbb3bfa63dbdd7f45fca18a4e
all4senses <all4senses@gmail.com>
2012-09-20 06:12:52 +0300

    Park modules and theme

--- modules/_Park/park_image/js/park_add_Colorbox_ToAlbums.js
+++ modules/_Park/park_image/js/park_add_Colorbox_ToAlbums.js
@@ -3,11 +3,11 @@
   Drupal.behaviors.park_addColorboxToAlbums = {
     attach: function (context, settings) {
        
-       console.log(Drupal.settings.park_image);
+       //console.log(Drupal.settings.park_image);
        
        for (group_class in Drupal.settings.park_image) {
          
-          console.log(group_class);
+          //console.log(group_class);
 
           //Assign the ColorBox event to elements
           jQuery("." + group_class).colorbox({transition:"fade", slideshow: true, slideshowAuto: false, rel: group_class, width: "95%", height: "95%"});
