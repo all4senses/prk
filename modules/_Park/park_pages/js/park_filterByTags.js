edbae56bbbf3fb72b58fe1b8a1187f4cb0338078
all4senses <all4senses@gmail.com>
2012-09-24 08:43:14 +0300

    Filter by tags

--- modules/_Park/park_pages/js/park_filterByTags.js
+++ modules/_Park/park_pages/js/park_filterByTags.js
@@ -8,6 +8,16 @@
        $("#doFilterByTag").click(function(){
          console.log($("#contentUrl").val());
          console.log($("#contentOpened_query").val());
+         var tags = jQuery('.chzn-select').val().toString();
+         if (tags) {
+          if ($("#contentOpened_query").val()) {
+            top.location.href = $("#contentUrl").val() + '&tags=' + tags;
+          }
+          else {
+            top.location.href = $("#contentUrl").val() + '?tags=' + tags;
+          }
+         }
+         
        });
        
 
