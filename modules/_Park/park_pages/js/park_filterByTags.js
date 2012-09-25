37f88f328a2b4d16df84a179d0949cd3f0b376a1
all4senses <all4senses@gmail.com>
2012-09-25 19:37:15 +0300

    Filter by tags

--- modules/_Park/park_pages/js/park_filterByTags.js
+++ modules/_Park/park_pages/js/park_filterByTags.js
@@ -9,8 +9,12 @@
        
       query_opened = false;
       source_param_string = '';
+      final_param_string = '';
       var params = {};
       
+      // console.log('top.location.href = ' + decodeURIComponent(top.location.href));
+      // 'onChange' => "top.location.href='http://getvoip.com/" . $_GET['q'] . "?provider=' + encodeURIComponent(document.getElementById('select_provider').options[document.getElementById('select_provider').selectedIndex].value) + '$url'"),
+
       current_query = location.search;
       console.log('current_query = ' + current_query);
       
@@ -42,27 +46,53 @@
       
       
       
-      function getTagsQueryPart() {
-        return 'tags=xxx';
-      }
       
       
       $("#doFilterByTag").click(function(){
         
-        test_tag = 'zzzzzz ' + getTagsQueryPart();
-        console.log('test_tag = ' + test_tag);
-          
+        tagsQueryPart = getTagsQueryPart();
+        authorQueryPart = getAuthorQueryPart();
+        
+        console.log('tagsQueryPart = ' + tagsQueryPart);
+        
+        if (tagsQueryPart) {
+          final_param_string = final_param_string + (final_param_string ? '&' : '?') + tagsQueryPart;
+        }
+        if (authorQueryPart) {
+          final_param_string = final_param_string + (final_param_string ? '&' : '?') + authorQueryPart;
+        }
+        
+        final_url = location.origin + location.pathname + final_param_string;
+
+        console.log('final url with selected tags' + final_url);
+        
+        ////top.location.href = new_url;
+
+      });
+      
+      
+      
+      
+      
+      function getAuthorQueryPart() {
+        authorQueryPart = 'author=aaaaaaa';
+        return authorQueryPart;
+      }
+
+
+
+      
+      function getTagsQueryPart() {
+        
         param_string = source_param_string;
         tags = '';
         tagsDisabled = ''; 
         
         if (jQuery('.chzn-select').val()) {
-        tags = jQuery('.chzn-select').val().toString();
+          tags = jQuery('.chzn-select').val().toString();
         }
         console.log('tags = ' + tags);
 
-        //str = ''; jQuery('.chzn-select [selected][disabled]').each(function(){str = str + (str ? ',' : '') + jQuery(this).val()}); console.log(str);
-        
         jQuery('.chzn-select [selected][disabled]').each(
           function(){tagsDisabled = tagsDisabled + (tagsDisabled ? ',' : '') + jQuery(this).val()
         }); 
@@ -71,28 +101,15 @@
 
         console.log('tagsDisabled = ' + tagsDisabled);
         console.log('tagsAll = ' + tags);
-        //console.log('top.location.href = ' + decodeURIComponent(top.location.href));
-
-
-        // 'onChange' => "top.location.href='http://getvoip.com/" . $_GET['q'] . "?provider=' + encodeURIComponent(document.getElementById('select_provider').options[document.getElementById('select_provider').selectedIndex].value) + '$url'"),
-        tags = encodeURIComponent(tags);
-        if (tags) {
-          if (query_opened) {
-            param_string = param_string + '&tags=' + tags;
-          }
-          else {
-            param_string = param_string + '?tags=' + tags;
-          }
-          query_opened = true;
-        }
-
-        new_url = location.origin + location.pathname + param_string;
-
-        console.log('final url with selected tags' + new_url);
         
-        ////top.location.href = new_url;
-
-      });
+        if (tags) {
+          return 'tags=' + encodeURIComponent(tags);
+        }
+        
+        return false;
+      }
+      
+      
        
 
     }
