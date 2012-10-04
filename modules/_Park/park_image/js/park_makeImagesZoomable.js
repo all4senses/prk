0b0fcb018754f9b8f3b55fd80ea809639c1a4195
all4senses <all4senses@gmail.com>
2012-10-04 05:34:12 +0300

    Images

--- modules/_Park/park_image/js/park_makeImagesZoomable.js
+++ modules/_Park/park_image/js/park_makeImagesZoomable.js
@@ -4,7 +4,8 @@
     attach: function (context, settings) {
        
        //$(".full-content img").colorbox({iframe:true, innerWidth:425, innerHeight:344});
-       $(".full-content img").colorbox({rel:"gallery"});
+       //$(".full-content img").colorbox({rel:"gallery"});
+       $(".full-content img").colorbox();
        console.log('full content!');
        
     }
