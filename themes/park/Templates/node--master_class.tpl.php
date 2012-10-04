81ca2a218f4264285b2261c5c682ae3f43daee55
all4senses <all4senses@gmail.com>
2012-10-04 21:48:35 +0300

    Users

--- themes/park/Templates/node--master_class.tpl.php
+++ themes/park/Templates/node--master_class.tpl.php
@@ -31,4 +31,19 @@ echo '</div>';
 if ($content['links'])
   echo '<div class="links">' . render($content['links']) . '</div>';
 
-print render($content['comments']);
+//print render($content['comments']);
+
+?>
+
+<div id="mc-container"></div>
+<script type="text/javascript">
+var mcSite = '10053';
+(function() {
+    var mc = document.createElement('script');
+    mc.type = 'text/javascript';
+    mc.async = true;
+    mc.src = 'http://cackle.me/mc.widget-min.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(mc);
+})();
+</script>
+<a href="http://cackle.me" id="mc-link">система комментирования <b style="color:#4FA3DA">CACKL</b><b style="color:#F65077">E</b></a>
