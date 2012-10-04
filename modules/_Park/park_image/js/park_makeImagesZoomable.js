c9b4e1b33f22c11cf5bac45c6fda41f03ba3c2a6
all4senses <all4senses@gmail.com>
2012-10-04 05:31:37 +0300

    Images

--- modules/_Park/park_image/js/park_makeImagesZoomable.js
+++ modules/_Park/park_image/js/park_makeImagesZoomable.js
@@ -3,7 +3,8 @@
   Drupal.behaviors.park_makeImagesZoomable = {
     attach: function (context, settings) {
        
-       $(".full-content img").colorbox({iframe:true, innerWidth:425, innerHeight:344});
+       //$(".full-content img").colorbox({iframe:true, innerWidth:425, innerHeight:344});
+       $(".full-content img").colorbox({rel:"gallery"});
        console.log('full content!');
        
     }
