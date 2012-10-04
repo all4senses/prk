e8b4716423919cacd7a00dd3040b93cea9ce9771
all4senses <all4senses@gmail.com>
2012-10-04 06:11:48 +0300

    Images

--- modules/_Park/park_image/js/park_makeImagesZoomable.js
+++ modules/_Park/park_image/js/park_makeImagesZoomable.js
@@ -5,7 +5,9 @@
        
        //$(".full-content img").colorbox({iframe:true, innerWidth:425, innerHeight:344});
        //$(".full-content img").colorbox({rel:"gallery"});
-       $(".full-content img").css({'cursor' : 'pointer', 'cursor' : '-webkit-zoom-in', 'cursor' : '-moz-zoom-in'}).colorbox({
+       //$(".full-content img").css({'cursor' : 'pointer', 'cursor' : '-webkit-zoom-in', 'cursor' : '-moz-zoom-in'});
+       $(".full-content img").css({'cursor' : '-webkit-zoom-in', 'cursor' : '-moz-zoom-in'});
+       $(".full-content img").colorbox({
          //href: this.src
          width:"80%", 
          height:"80%",
