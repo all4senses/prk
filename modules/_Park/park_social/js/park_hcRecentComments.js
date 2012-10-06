7ade66616b125cfaa79f67c7152010d4476a80fd
all4senses <all4senses@gmail.com>
2012-10-06 12:12:57 +0300

    HC blocks

--- modules/_Park/park_social/js/park_hcRecentComments.js
+++ modules/_Park/park_social/js/park_hcRecentComments.js
@@ -1,21 +1,8 @@
-(function ($) {
-
-  Drupal.behaviors.park_hcRecentComments = {
-    attach: function (context, settings) {
        
       var _hcwp = _hcwp || [];
       _hcwp.push({widget_id : 3235, widget : "Mixstream",filter:"last",limit:5, append:"#hypercomments_recent" });
       (function() {
       var hcc = document.createElement("script"); hcc.type = "text/javascript"; hcc.async = true;
       hcc.src = ("https:" == document.location.protocol ? "https" : "http")+"://widget.hypercomments.com/apps/js/hc.dev.js";
-      
-      //var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hcc, s.nextSibling);
-      
-      document.getElementById("hypercomments_recent").insertBefore(hcc);
-      
+      var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hcc, s.nextSibling);
       })();
-
-    }
-  };
-
-}(jQuery));
