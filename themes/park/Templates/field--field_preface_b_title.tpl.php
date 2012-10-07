41560dffc6eaca6de635d21ba99257a0861ac0e8
all4senses <all4senses@gmail.com>
2012-10-07 03:23:45 +0300

    User Theming

--- themes/park/Templates/field--field_preface_b_title.tpl.php
+++ themes/park/Templates/field--field_preface_b_title.tpl.php
@@ -42,10 +42,4 @@
  * @see theme_field()
  */
 ?>
-<!--
-THIS FILE IS NOT USED AND IS HERE AS A STARTING POINT FOR CUSTOMIZATION ONLY.
-See http://api.drupal.org/api/function/theme_field/7 for details.
-After copying this file to your theme's folder and customizing it, remove this
-HTML comment.
--->
 <h2 class="preface"><?php echo render($items[0]); ?></h2>
