caf3ab65ed1f22581296eab3ff3a2ed6a78be414
all4senses <all4senses@gmail.com>
2012-10-13 20:02:26 +0300

    Sequence Slideshow

---
+++ modules/_Park/park_image/js/park_add_sequenceSlideshow.js
@@ -0,0 +1,16 @@
+(function ($) {
+  Drupal.behaviors.park_add_sequenceSlideshow = {
+    attach: function (context, settings) {
+
+       console.log('in park_add_sequenceSlideshow...');
+       
+//       for (group_class in Drupal.settings.park_image) {
+//         
+//          //console.log(group_class);
+//
+//       }
+
+    }
+  };
+  
+}(jQuery));
