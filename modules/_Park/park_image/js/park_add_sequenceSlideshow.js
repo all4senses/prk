db5371d6f90335ff787afc3c6aca10ef78266d8e
all4senses <all4senses@gmail.com>
2012-10-13 20:39:58 +0300

    Sequence Slideshow

--- modules/_Park/park_image/js/park_add_sequenceSlideshow.js
+++ modules/_Park/park_image/js/park_add_sequenceSlideshow.js
@@ -2,7 +2,38 @@
   Drupal.behaviors.park_add_sequenceSlideshow = {
     attach: function (context, settings) {
 
-        var sequence = $("#sequence").sequence().data("sequence");
+        var options = {
+          nextButton: true,
+          prevButton: true,
+          animateStartingFrameIn: true,
+          transitionThreshold: 250,
+          preloadTheseFrames: [1],
+          preloadTheseImages: [
+            "images/tn-model1.png",
+            "images/tn-model2.png",
+            "images/tn-model3.png"
+          ]
+        };
+
+        var sequence = $("#sequence").sequence(options).data("sequence");
+
+        sequence.afterLoaded = function(){
+          $("#nav").fadeIn(100);
+          $("#nav li:nth-child("+(sequence.settings.startingFrameID)+") img").addClass("active");
+        }
+
+        sequence.beforeNextFrameAnimatesIn = function(){
+          $("#nav li:not(:nth-child("+(sequence.nextFrameID)+")) img").removeClass("active");
+          $("#nav li:nth-child("+(sequence.nextFrameID)+") img").addClass("active");
+        }
+
+        $("#nav li").click(function(){
+          if(!sequence.active){
+            $(this).children("img").removeClass("active").children("img").addClass("active");
+            sequence.nextFrameID = $(this).index()+1;
+            sequence.goTo(sequence.nextFrameID);
+          }
+        });
         
         console.log('in park_add_sequenceSlideshow...');
        
