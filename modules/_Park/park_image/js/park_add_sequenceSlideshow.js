e4ad1a0566574f0a0cc5b9974b18459c7237e5ab
all4senses <all4senses@gmail.com>
2012-10-13 20:24:52 +0300

    Sequence Slideshow

--- modules/_Park/park_image/js/park_add_sequenceSlideshow.js
+++ modules/_Park/park_image/js/park_add_sequenceSlideshow.js
@@ -2,7 +2,10 @@
   Drupal.behaviors.park_add_sequenceSlideshow = {
     attach: function (context, settings) {
 
-       console.log('in park_add_sequenceSlideshow...');
+        var sequence = $("#sequence").sequence().data("sequence");
+        
+        console.log('in park_add_sequenceSlideshow...');
+       
        
 //       for (group_class in Drupal.settings.park_image) {
 //         
