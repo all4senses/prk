20101fe9e7a0beb1f5d2f4c8a26659eb06d1948f
all4senses <all4senses@gmail.com>
2012-10-14 17:51:29 +0300

    Sequence Slideshow

--- modules/_Park/park_image/js/park_add_advancedSlider.js
+++ modules/_Park/park_image/js/park_add_advancedSlider.js
@@ -3,26 +3,23 @@
     attach: function (context, settings) {
 
 
-                $('#responsive-slider').advancedSlider({width: '80%',
-												height: '70%',
+               $('#responsive-slider').advancedSlider({width: '80%',
+												height: '60%',
 												scaleType: 'outsideFit',
-												skin: 'glossy-square-gray',
+												skin: 'glossy-square-blue',
 												effectType: 'swipe',
 												pauseSlideshowOnHover: true,
 												swipeThreshold: 50,
-												slideButtons: false,
-												thumbnailType: 'scroller',
-												thumbnailButtons: true,
-												thumbnailScrollerResponsive: true,
-												minimumVisibleThumbnails: 2,
-											    slideProperties:{
+												slideArrowsToggle: false,
+												slideProperties:{
 													0:{captionSize: 35, captionHideEffect: 'slide'},
 													1:{captionPosition: 'custom', captionShowEffect: 'fade', captionHeight: 160, captionLeft: 70, slideshowDelay: 10000},
 													3:{captionSize: 40},
 													5:{captionPosition: 'left', captionSize: 150, captionHideEffect: 'slide'},
 													7:{captionPosition: 'right', captionSize: 150, captionHideEffect: 'slide'}
 												}
-                });
+            		});
+
 
                 // set the initial height of the slider to 50% from the width
                 $('#responsive-slider').css('height', $('#responsive-slider').width() * 0.50);
