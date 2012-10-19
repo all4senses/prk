e35f95c2af8182c1a4ae826f966491fc46322c7e
all4senses <all4senses@gmail.com>
2012-10-19 20:55:01 +0300

    Social buttons

--- libraries/jquery.plugins/socialite/socialite.js
+++ libraries/jquery.plugins/socialite/socialite.js
@@ -806,6 +806,47 @@ window.Socialite = (function(window, document, undefined)
         }
     });
     
+    
+    
+    
+    
+    Socialite.network('mailru', {
+        script: {
+            src     : '//cdn.connect.mail.ru/js/loader.js',
+            charset : 'utf-8'
+        }
+        
+    });
+
+    /**
+     * Add a unique widget to the network
+     * Socialite will activate elements with a class name of `network_name-widget_name`, e.g. `twitter-share`
+     */
+    Socialite.widget('mailru', 'like', {
+        reappend: null,
+        init: function(instance)
+        {
+            var el = document.createElement('a');
+            el.className = 'mailru-like';
+            Socialite.copyDataAttributes(instance.el, el);
+            el.setAttribute('href', instance.el.getAttribute('data-href'));
+            instance.el.appendChild(el);
+        },
+        activate: function(instance)
+        {
+            var w = instance.widget,
+                n = w.network.name;
+            if (Socialite.networkReady(n)) {
+                if (w.reappend) {
+                    clearTimeout(w.reappend);
+                }
+                w.reappend = setTimeout(function() {
+                    Socialite.reloadNetwork(n);
+                }, 50);
+            }
+        }
+    });
+    
 
 })(window, window.document, window.Socialite);
 
