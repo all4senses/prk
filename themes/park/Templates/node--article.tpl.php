8a3e2a40d64262d663446f7385db3f6dee38888c
all4senses <all4senses@gmail.com>
2012-10-20 07:44:36 +0300

    Social buttons

--- themes/park/Templates/node--article.tpl.php
+++ themes/park/Templates/node--article.tpl.php
@@ -153,7 +153,6 @@
 
                         <div class="addthis_toolbox addthis_default_style" addthis:url="<?php echo $url?>">
                           <a href="http://addthis.com/bookmark.php?v=250&amp;pub=all4senses"></a>
-                          <a class="addthis_button_email" title="E-mail this page link"></a>
                           <a class="addthis_button_compact"></a>
                         </div>
                         <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pub=all4senses"></script>
