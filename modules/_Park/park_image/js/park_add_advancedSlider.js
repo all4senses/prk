31ff53dbdcd084cc310ad060614f80666ab110a0
all4senses <all4senses@gmail.com>
2012-10-21 02:21:05 +0300

    Home page

--- modules/_Park/park_image/js/park_add_advancedSlider.js
+++ modules/_Park/park_image/js/park_add_advancedSlider.js
@@ -11,7 +11,7 @@
 												//height: '70%',
                         
                         //width: '80%',
-                        height: '25%',
+                        height: '100',
                         scaleType: 'insideFit',
                         
 												//scaleType: 'proportionalFit', //'insideFit', 'exactFit',
