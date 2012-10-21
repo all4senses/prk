88546f5cc204c135c586aa789e90a46728e747e5
all4senses <all4senses@gmail.com>
2012-10-21 08:15:36 +0300

    Home page

--- modules/_Park/park_image/js/park_add_advancedSlider.js
+++ modules/_Park/park_image/js/park_add_advancedSlider.js
@@ -10,7 +10,7 @@
                  
                  
                         width: '100%', 
-									 		 height: '20%', 
+									 		 height: '10%', 
 											 skin: 'minimal-small', 
 										   	 shuffle: true, 
 										   	 slideshowControls: true, 
